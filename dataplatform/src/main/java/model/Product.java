feadd186146f1aec02aa04acc9dd38991a5f1cf8
gseversen <geversenn@gmail.com>
2026-01-14 20:59:20 -0600

    Product class added with attributes and getters

---
+++ dataplatform/src/main/java/model/Product.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Product {
+
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
+    private int stock;
+    private String availability;
+
+    public Product(String name, String brand, String category, double price, int stock, String availability) {
+        this.name = name;
+        this.brand = brand;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+        this.availability = availability;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+}
